5ee261d2dc49aa7f3c361709198698f192c615a8
Jeferson Labout <Jefson1989@gmail.com>
2022-05-05 10:52:13 -0300

    Entity

---
+++ src/main/java/com/jeferson/santander/bankline/api/model/Movimentacao.java
@@ -0,0 +1,65 @@
+package com.jeferson.santander.bankline.api.model;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tab_movimentacao")
+public class Movimentacao {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;	
+	private LocalDateTime dataHora;
+	private String descricao;
+	private Double valor;
+	
+	@Enumerated
+	private MovimentacaoTipo tipo;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
+
+	public void setDataHora(LocalDateTime dataHora) {
+		this.dataHora = dataHora;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public MovimentacaoTipo getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(MovimentacaoTipo tipo) {
+		this.tipo = tipo;
+	}
+
+}
---
+++ src/main/java/com/jeferson/santander/bankline/api/model/MovimentacaoTipo.java
@@ -0,0 +1,8 @@
+package com.jeferson.santander.bankline.api.model;
+
+public enum MovimentacaoTipo {
+	
+	RECEITA,
+	DESPESA
+
+}
