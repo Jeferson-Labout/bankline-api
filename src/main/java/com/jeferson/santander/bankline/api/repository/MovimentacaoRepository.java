25cbeaf772000ea2c9a4941dead32eb4b57772b2
Jeferson-Labout <jefson1989@gmail.com>
2022-05-08 17:04:10 -0300

    put

--- src/main/java/com/jeferson/santander/bankline/api/repository/MovimentacaoRepository.java
+++ src/main/java/com/jeferson/santander/bankline/api/repository/MovimentacaoRepository.java
@@ -1,9 +1,11 @@
 package com.jeferson.santander.bankline.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jeferson.santander.bankline.api.model.Movimentacao;
 
 public interface MovimentacaoRepository extends JpaRepository<Movimentacao, Long>{
-
+	 public List<Movimentacao>findByIdConta(Integer idConta);
 }
